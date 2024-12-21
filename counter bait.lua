19d3d3ffd322e6c11bd70a00130551bf1c47f755
Selunar <156382268+Selunariorium@users.noreply.github.com>
2024-12-21 17:45:08 +0700

    Update counter bait.lua

--- counter bait.lua
+++ counter bait.lua
@@ -1,44 +1,33 @@
-shared.message = 'Simon says counter.';
 local counterids: table = {
-	'rbxassetid://13532562418',
-	'rbxassetid://15311685628'
+    'rbxassetid://13532562418',
+    'rbxassetid://15311685628'
 }
-local players = {}
-
 local verylegit: boolean = false;
 local isAlive = function(v)
-	return workspace.Live:FindFirstChild(v.Name) and v and v.PrimaryPart and v:FindFirstChildWhichIsA('Humanoid') and v.Humanoid.Health ~= 0;
+    local suc, res = pcall(function()
+        return v.Character and v.Character.PrimaryPart and v.Character:FindFirstChildWhichIsA('Humanoid') and v.Character.Humanoid.Health ~= 0;
+    end)
+    return suc and res or suc
 end
-
 local func: (Player) -> () = function(v: Player): ()
-	assert(v, 'First argument is nil.')
-	if not isAlive(v) then
-		repeat task.wait() until isAlive(v)
-		task.wait()
-	end
-
-	if v ~= game.Players.LocalPlayer and v.Character and v.Character:FindFirstChildWhichIsA('Humanoid') and v.Character.Humanoid.Health ~= 0 then
-		v.Character.Humanoid.AnimationPlayed:Connect(function(anim: Animation): ()
-			if isAlive(game.Players.LocalPlayer) and table.find(counterids, anim.Animation.AnimationId) and (v.Character.PrimaryPart.Position - game.Players.LocalPlayer.Character.PrimaryPart.Position).Magnitude <= 5.5 then
-				if verylegit then print('naw') return end
-				verylegit = true;
-				verylegit = false;
-				game:GetService('ReplicatedStorage').DefaultChatSystemChatEvents.SayMessageRequest:FireServer(shared.message or 'Simon says counter.', 'All');
-			end;
-		end);
-	end;
+    assert(v, 'First argument is nil.')
+    if not isAlive(v) then
+        repeat task.wait() until workspace.Live:FindFirstChild(v.Name) and workspace.Live:FindFirstChild(v.Name):FindFirstChild('HumanoidRootPart') and workspace.Live:FindFirstChild(v.Name):FindFirstChild('Humanoid') and workspace.Live:FindFirstChild(v.Name).Humanoid.Health ~= 0
+    end
+    if v ~= game.Players.LocalPlayer then
+        v.Character.Humanoid.AnimationPlayed:Connect(function(anim: Animation): ()
+            if isAlive(game.Players.LocalPlayer) and table.find(counterids, anim.Animation.AnimationId) and (v.Character.PrimaryPart.Position - game:GetService('Players').LocalPlayer.Character.PrimaryPart.Position).Magnitude <= shared.Range then
+                if verylegit then return print('naw') end;
+                verylegit = true;
+                task.delay(0.05, function()
+                    verylegit = false;
+                    game:GetService('ReplicatedStorage').DefaultChatSystemChatEvents.SayMessageRequest:FireServer(shared.message or 'real', 'All');
+                end)
+            end;
+        end);
+    end;
 end
-
 for i: number, v: Player in game:GetService('Players'):GetPlayers() do
-	func(v)
-	v.CharacterAdded:Connect(func)
-	table.insert(players, v.Name)
-end
-
-game:GetService('Players').PlayerAdded:Connect(function()
-	if v ~= game:GetService('Players').LocalPlayer and not table.find(players, v.Name) then
-		func(v)
-		v.CharacterAdded:Connect(func)
-		table.insert(players, v.Name)
-	end
-end)
+    func(v)
+    v.CharacterAdded:Connect(func)
+end;
